5d29e4409a1ca9eaf7aa02520ca99aed4a9f4168
touhourl <touhourl@proton.me>
2026-08-06 05:56:33 +0000

    feat: gameplay objects to public simulation values
     * copy projectile, input, clear, item, damage, enemy flags

--- src/sim/sim.c
+++ src/sim/sim.c
@@ -209,3 +209,83 @@ static void fill_boss_state(TaiseiSim *sim, TaiseiSimBossState *out) {
     out->spell_captured = ATTACK_IS_SPELL(attack->type) && attack_has_finished(attack) && !attack_was_failed(attack);
 }
 
+static uint32_t projectile_category(const Projectile *p) {
+    switch(p->type) {
+        case PROJ_ENEMY: return TAISEI_SIM_PROJECTILE_ENEMY;
+        case PROJ_DEAD: return TAISEI_SIM_PROJECTILE_CLEARING;
+        case PROJ_PLAYER: return TAISEI_SIM_PROJECTILE_PLAYER;
+        default: return TAISEI_SIM_PROJECTILE_INVALID;
+    }
+}
+
+static uint32_t projectile_flags(const Projectile *p) {
+    uint32_t flags = 0;
+    if(!(p->flags & PFLAG_NOGRAZE)) flags |= TAISEI_SIM_PROJECTILE_FLAG_GRAZEABLE;
+    if(!(p->flags & PFLAG_NOCLEAR)) flags |= TAISEI_SIM_PROJECTILE_FLAG_CLEARABLE;
+    if(!(p->flags & PFLAG_NOCOLLISION)) flags |= TAISEI_SIM_PROJECTILE_FLAG_COLLISION_ENABLED;
+    if(p->flags & PFLAG_INDESTRUCTIBLE) flags |= TAISEI_SIM_PROJECTILE_FLAG_INDESTRUCTIBLE;
+    if(p->type == PROJ_ENEMY && !(p->flags & PFLAG_NOCOLLISION)) flags |= TAISEI_SIM_PROJECTILE_FLAG_ACTIVE_HAZARD;
+    return flags;
+}
+
+static uint32_t input_flags(PlrInputFlag flags) {
+    uint32_t out = 0;
+    if(flags & INFLAG_UP) out |= TAISEI_SIM_INPUT_UP;
+    if(flags & INFLAG_DOWN) out |= TAISEI_SIM_INPUT_DOWN;
+    if(flags & INFLAG_LEFT) out |= TAISEI_SIM_INPUT_LEFT;
+    if(flags & INFLAG_RIGHT) out |= TAISEI_SIM_INPUT_RIGHT;
+    if(flags & INFLAG_FOCUS) out |= TAISEI_SIM_INPUT_FOCUS;
+    if(flags & INFLAG_SHOT) out |= TAISEI_SIM_INPUT_SHOT;
+    if(flags & INFLAG_SKIP) out |= TAISEI_SIM_INPUT_SKIP;
+    return out;
+}
+
+static uint32_t clear_flags(uint internal_flags) {
+    uint32_t out = 0;
+    if(internal_flags & CLEAR_HAZARDS_BULLETS) out |= TAISEI_SIM_CLEAR_FLAG_BULLETS;
+    if(internal_flags & CLEAR_HAZARDS_LASERS) out |= TAISEI_SIM_CLEAR_FLAG_LASERS;
+    if(internal_flags & CLEAR_HAZARDS_FORCE) out |= TAISEI_SIM_CLEAR_FLAG_FORCED;
+    if(internal_flags & CLEAR_HAZARDS_NOW) out |= TAISEI_SIM_CLEAR_FLAG_IMMEDIATE;
+    if(internal_flags & CLEAR_HAZARDS_SPAWN_VOLTAGE) out |= TAISEI_SIM_CLEAR_FLAG_SPAWNS_VOLTAGE;
+    return out;
+}
+
+static uint32_t item_type(ItemType type) {
+    switch(type) {
+        case ITEM_PIV: return TAISEI_SIM_ITEM_PIV;
+        case ITEM_POINTS: return TAISEI_SIM_ITEM_POINTS;
+        case ITEM_POWER_MINI: return TAISEI_SIM_ITEM_POWER_MINI;
+        case ITEM_POWER: return TAISEI_SIM_ITEM_POWER;
+        case ITEM_SURGE: return TAISEI_SIM_ITEM_SURGE;
+        case ITEM_VOLTAGE: return TAISEI_SIM_ITEM_VOLTAGE;
+        case ITEM_BOMB_FRAGMENT: return TAISEI_SIM_ITEM_BOMB_FRAGMENT;
+        case ITEM_LIFE_FRAGMENT: return TAISEI_SIM_ITEM_LIFE_FRAGMENT;
+        case ITEM_BOMB: return TAISEI_SIM_ITEM_BOMB;
+        case ITEM_LIFE: return TAISEI_SIM_ITEM_LIFE;
+    }
+    return 0;
+}
+
+static uint32_t damage_type(DamageType type) {
+    switch(type) {
+        case DMG_ENEMY_SHOT: return TAISEI_SIM_DAMAGE_ENEMY_SHOT;
+        case DMG_ENEMY_COLLISION: return TAISEI_SIM_DAMAGE_ENEMY_COLLISION;
+        case DMG_PLAYER_SHOT: return TAISEI_SIM_DAMAGE_PLAYER_SHOT;
+        case DMG_PLAYER_BOMB: return TAISEI_SIM_DAMAGE_PLAYER_BOMB;
+        case DMG_PLAYER_DISCHARGE: return TAISEI_SIM_DAMAGE_PLAYER_DISCHARGE;
+        default: return TAISEI_SIM_DAMAGE_UNDEFINED;
+    }
+}
+
+static uint32_t enemy_flags(Enemy *e) {
+    uint32_t flags = 0;
+    if(e->flags & EFLAG_KILLED) flags |= TAISEI_SIM_ENEMY_FLAG_KILLED;
+    if(enemy_is_targetable(e)) flags |= TAISEI_SIM_ENEMY_FLAG_TARGETABLE;
+    if(enemy_is_vulnerable(e)) flags |= TAISEI_SIM_ENEMY_FLAG_DAMAGEABLE;
+    if(!(e->flags & EFLAG_NO_HURT)) flags |= TAISEI_SIM_ENEMY_FLAG_HARMFUL;
+    if(e->flags & EFLAG_INVULNERABLE) flags |= TAISEI_SIM_ENEMY_FLAG_INVULNERABLE;
+    if(e->flags & EFLAG_IMPENETRABLE) flags |= TAISEI_SIM_ENEMY_FLAG_IMPENETRABLE;
+    if(e->flags & EFLAG_NO_AUTOKILL) flags |= TAISEI_SIM_ENEMY_FLAG_NO_AUTOKILL;
+    return flags;
+}
+
